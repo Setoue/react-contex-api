250f0cd007455c371e2ae1f0d5faa71afc19ca6f
Shigeru <proshigeru@gmail.com>
2023-09-06 16:01:10 -0300

    refactor: ajusted user context

--- src/common/context/User.js
+++ src/common/context/User.js
@@ -1,6 +1,7 @@
 import { createContext, useState } from "react";
 
 export const UserContext = createContext();
+UserContext.displayName = "Usuário";
 
 const UserProvider = ({ children }) => {
   const [name, setName] = useState("");
