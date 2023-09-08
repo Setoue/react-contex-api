7ad36359479b864914e193935361a1d4e10ddecf
Shigeru <proshigeru@gmail.com>
2023-09-08 14:30:26 -0300

    feat: added state valueTotalCart

--- src/common/context/Cart.js
+++ src/common/context/Cart.js
@@ -8,9 +8,17 @@ CartContext.displayName = "Cart";
 export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
   const [amountProduct, setAmountProduct] = useState(0);
+  const [valueTotalCart, setValueTotalCart] = useState(0);
   return (
     <CartContext.Provider
-      value={{ cart, setCart, amountProduct, setAmountProduct }}
+      value={{
+        cart,
+        setCart,
+        amountProduct,
+        setAmountProduct,
+        valueTotalCart,
+        setValueTotalCart,
+      }}
     >
       {children}
     </CartContext.Provider>
