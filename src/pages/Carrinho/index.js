1f511d516821787e41085f55fd0fcff10517092a
Shigeru <proshigeru@gmail.com>
2023-09-08 11:01:08 -0300

    refactor: going page cart and passing each product and your respective amount

--- src/pages/Carrinho/index.js
+++ src/pages/Carrinho/index.js
@@ -1,33 +1,48 @@
-import { Button, Snackbar, InputLabel } from '@material-ui/core';
-import MuiAlert from '@material-ui/lab/Alert';
-import { useState } from 'react';
-import { Container, Voltar, TotalContainer, PagamentoContainer} from './styles';
+import { Button, Snackbar, InputLabel } from "@material-ui/core";
+import MuiAlert from "@material-ui/lab/Alert";
+import { useState } from "react";
+import {
+  Container,
+  Voltar,
+  TotalContainer,
+  PagamentoContainer,
+} from "./styles";
+import { useCartContext } from "common/hooks/useCartContext";
+import Produto from "components/Produto";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 function Carrinho() {
   const [openSnackbar, setOpenSnackbar] = useState(false);
+  const { cart } = useCartContext();
+  const history = useHistory();
   return (
     <Container>
-      <Voltar />
-      <h2>
-        Carrinho
-      </h2>
+      <Voltar
+        onClick={() => {
+          history.goBack();
+        }}
+      />
+      <h2>Carrinho</h2>
+      {cart.map((product) => (
+        <Produto {...product} key={product.id} />
+      ))}
       <PagamentoContainer>
         <InputLabel> Forma de Pagamento </InputLabel>
       </PagamentoContainer>
       <TotalContainer>
-          <div>
-            <h2>Total no Carrinho: </h2>
-            <span>R$ </span>
-          </div>
-          <div>
-            <h2> Saldo: </h2>
-            <span> R$ </span>
-          </div>
-          <div>
-            <h2> Saldo Total: </h2>
-            <span> R$ </span>
-          </div>
-        </TotalContainer>
+        <div>
+          <h2>Total no Carrinho: </h2>
+          <span>R$ </span>
+        </div>
+        <div>
+          <h2> Saldo: </h2>
+          <span> R$ </span>
+        </div>
+        <div>
+          <h2> Saldo Total: </h2>
+          <span> R$ </span>
+        </div>
+      </TotalContainer>
       <Button
         onClick={() => {
           setOpenSnackbar(true);
@@ -35,27 +50,22 @@ function Carrinho() {
         color="primary"
         variant="contained"
       >
-         Comprar
-       </Button>
-        <Snackbar
-          anchorOrigin={
-            { 
-              vertical: 'top',
-              horizontal: 'right'
-            }
-          }
-          open={openSnackbar}
-          onClose={() => setOpenSnackbar(false)}
-        >
-           <MuiAlert
-            onClose={() => setOpenSnackbar(false)}
-            severity="success"
-          >
-            Compra feita com sucesso!
-          </MuiAlert>
-        </Snackbar>
+        Comprar
+      </Button>
+      <Snackbar
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        open={openSnackbar}
+        onClose={() => setOpenSnackbar(false)}
+      >
+        <MuiAlert onClose={() => setOpenSnackbar(false)} severity="success">
+          Compra feita com sucesso!
+        </MuiAlert>
+      </Snackbar>
     </Container>
-  )
+  );
 }
 
 export default Carrinho;
--- src/pages/Feira/NavBar/index.js
+++ src/pages/Feira/NavBar/index.js
@@ -4,13 +4,20 @@ import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import IconButton from "@material-ui/core/IconButton";
 import Badge from "@material-ui/core/Badge";
 import { useCartContext } from "common/hooks/useCartContext";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 export default function NavBar() {
   const { amountProduct } = useCartContext();
+  const history = useHistory();
   return (
     <Nav>
       <Logo />
-      <IconButton disabled={amountProduct === 0}>
+      <IconButton
+        disabled={amountProduct === 0}
+        onClick={() => {
+          history.push("/carrinho");
+        }}
+      >
         <Badge color="primary" badgeContent={amountProduct}>
           <ShoppingCartIcon />
         </Badge>
